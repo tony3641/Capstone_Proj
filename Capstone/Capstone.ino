10db253ff588f5f8a2f3f033f1e43f8701a72c64
Tony Han <tony3641@126.com>
2022-01-24 21:47:24 -0500

    Accelerometer lib added

--- Capstone/Capstone.ino
+++ Capstone/Capstone.ino
@@ -1,29 +1,48 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
+* SAUSS-ware embedded software
+* This is an OSU ECE Capstone project.
+* Team member: Tony Han, Dominic Oliverio, Colin Lee, Doug Van Arsdale, and Molly Loughridge
  */
 
 
 // the setup function runs once when you press reset or power the board
+
+#include <DFRobot_LIS2DW12.h>
+#include <DFRobot_LIS2DH12.h>
+#include <DFRobot_LIS.h>
+DFRobot_H3LIS200DL_I2C acce;
+
+#if defined(ESP32) || defined(ESP8266)
+#define H3LIS200DL_CS  D3
+#elif defined(__AVR__) || defined(ARDUINO_SAM_ZERO)
+#define H3LIS200DL_CS 3
+#elif (defined NRF5)
+#define H3LIS200DL_CS 2  //The pin on the development board with the corresponding silkscreen printed as P2 
+#endif
+
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  while (!acce.begin()) {
+	  Serial.println("I2C initialization failed.");
+	  delay(1000);
+  }
+  Serial.print("Chip ID=");
+  Serial.println(acce.getID(), HEX);
+  acce.setRange(DFRobot_LIS::eH3lis200dl_200g);
+  acce.setAcquireRate(DFRobot_LIS::eNormal_50HZ);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  long ax, ay, az;
+  ax = acce.readAccX();
+  ay = acce.readAccY();
+  az = acce.readAccZ();
+  Serial.print("X=");
+  Serial.print(ax);
+  Serial.print("Y=");
+  Serial.print(ay);
+  Serial.print("Z=");
+  Serial.print(az);
+  delay(50);
 }
